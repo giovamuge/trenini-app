d0d7c933285b810ac81340197ac734d95f7bf522
Giovanni Mugelli <mugelli.giovanni@gmail.com>
2025-04-18 21:11:53 +0200

    first commit

---
+++ App.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Maui.Controls.PlatformConfiguration;
+using Microsoft.Maui.Controls.PlatformConfiguration.iOSSpecific;
+
+namespace TreniniApp;
+
+public class App : Microsoft.Maui.Controls.Application
+{
+    public App() { }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        var navigationPage = new Microsoft.Maui.Controls.NavigationPage(new MainPage());
+        navigationPage.On<iOS>().SetPrefersLargeTitles(true);
+        return new Window(navigationPage);
+    }
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,33 @@
+﻿using CommunityToolkit.Maui;
+using CommunityToolkit.Maui.Markup;
+using Microsoft.Extensions.Logging;
+
+namespace TreniniApp;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .UseMauiCommunityToolkit(options =>
+            {
+                options.SetShouldSuppressExceptionsInConverters(false);
+                options.SetShouldSuppressExceptionsInBehaviors(false);
+                options.SetShouldSuppressExceptionsInAnimations(false);
+            })
+            .UseMauiCommunityToolkitMarkup()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+
+        return builder.Build();
+    }
+}
